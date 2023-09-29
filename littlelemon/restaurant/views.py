5f55f945018103f23f4568b51f166ab1b4ab5fc6
Joao Fernandes <joao@cellcraft.com>
2023-09-29 12:12:55 +0100

    set up menu api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .serializers import MenuSerializer
+from .models import Menu
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+    
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
